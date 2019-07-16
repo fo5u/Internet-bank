286e48d60b87979e779e3881f8554d1c44092eb5
Aleksandr <fo5u1990@gmail.com>
2019-07-16 16:56:23 +0300

    New function

--- code_i_bank.py
+++ code_i_bank.py
@@ -1,13 +1,22 @@
+
+import internet_bank_except
+
 class InternetBank(object):
     balance = 5000
     attempts = 3
     user_can_get_money = False
 
     def top_up_money(self,my_money):
-        return self.balance + my_money
+        if my_money == -1:
+            raise ValueError
+        else:
+            return self.balance + my_money
 
     def enter_pin_code(self,pin_code):
         correct_pin_code = 333
+
+        # if pin_code == str
+        #     raise ValueError
 
         if self.attempts == 0:
             return "incorrect pin code"
@@ -20,18 +29,24 @@ class InternetBank(object):
             self.user_can_get_money = True
             return True
 
+
     def snyat_dengi(self, minus_money):
         if self.user_can_get_money:
+
+            if minus_money == -1:
+                raise ValueError
+
             if minus_money <= self.balance:
                 self.balance = self.balance - minus_money
                 return minus_money
             else:
                 return "Error"
 
-
-    def confirm_the_balance(self, show_balance):
-        if self.user_can_get_money:
-            if show_balance == "show balance":
+    def confirm_the_balance(self, my_balance):
+         if self.user_can_get_money:
+            if my_balance == 1:
                 return self.balance
-        else:
-            return "No access"
+            if my_balance!=1:
+                return "No access"
+
+
